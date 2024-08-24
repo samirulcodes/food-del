8fddc406cd937764af06b4e9175483aa7e9ea338
samirulcodes <samirulislam.official.com>
2024-08-24 12:22:42 +0530

    food del

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,15 @@ import ReactDom from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './context/StoreContext.jsx'
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+
   </BrowserRouter>
 
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/foodDisplay/FoodDisplay'
 
 const Home = () => {
 
@@ -13,6 +14,7 @@ const Home = () => {
 
       {/* passing category as a props */}
       <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   )
 }
